0a4b1d8ebd475f2e43e265de8207a0f753fb611f
EC2 Default User <ec2-user@ip-172-31-9-19.ap-northeast-1.compute.internal>
2023-02-25 17:59:46 +0000

    [Update]アプリケーション1 課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,23 +5,41 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books =Book.all
+    @books = Book.all
   end
   
   def create
     book = Book.new(book_params)
-    
-    book.save
-    
-    redirect_to'/books/:id'
-    
+    if book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(book.id)
+    else
+      @books =Book.all
+      @book = Book.new
+      render:index
+    end
   end
 
   def show
-    
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to'/books'
   end
   
   private
